d3de5745df426497d8f49f7b4228b7917a30f631
cihat askin <canali450@gmail.com>
2020-04-11 13:49:15 +0300

    model binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Cihat';
+  items= [
+    { description: 'Yazılım', action: 'No' },
+    { description: 'Kahvaltı', action: 'No' },
+    { description: 'Spor', action: 'No' },
+    { description: 'İngilizce', action: 'No' },
+  ];
 }
